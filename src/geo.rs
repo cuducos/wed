b6d455a102fd3eedf81a44744ed13453ef500898
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2023-05-21 09:14:08 -0400

    Adds tests to geo module

--- src/geo.rs
+++ src/geo.rs
@@ -42,3 +42,82 @@ pub async fn coordinates(query: &str) -> Result<(f64, f64)> {
         results[0].lon.parse::<f64>()?,
     ))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use float_cmp::approx_eq;
+    use wiremock::matchers::{method, path, query_param};
+    use wiremock::{Mock, MockServer, ResponseTemplate};
+
+    #[tokio::test]
+    async fn test_coordinates_success() {
+        let server = MockServer::start().await;
+
+        let query = "New York";
+        let expected_lat = "40.7128";
+        let expected_lon = "-74.0060";
+
+        let response_body = format!(r#"[{{"lat": "{expected_lat}", "lon": "{expected_lon}"}}]"#);
+
+        Mock::given(method("GET"))
+            .and(path("/search.php"))
+            .and(query_param("format", "jsonv2"))
+            .and(query_param("q", query))
+            .respond_with(
+                ResponseTemplate::new(200).set_body_raw(response_body, "application/json"),
+            )
+            .mount(&server)
+            .await;
+
+        let result = coordinates(query).await;
+        assert!(result.is_ok());
+
+        let (lat, lon) = result.unwrap();
+        approx_eq!(f64, lat, expected_lat.parse::<f64>().unwrap(), ulps = 2);
+        approx_eq!(f64, lon, expected_lon.parse::<f64>().unwrap(), ulps = 2);
+        server.verify().await;
+    }
+
+    #[tokio::test]
+    async fn test_coordinates_http_error() {
+        let server = MockServer::start().await;
+
+        let query = "InvalidQuery";
+
+        Mock::given(method("GET"))
+            .and(path("/search.php"))
+            .and(query_param("format", "jsonv2"))
+            .and(query_param("q", query))
+            .respond_with(ResponseTemplate::new(404))
+            .mount(&server)
+            .await;
+
+        let result = coordinates(query).await;
+        assert!(result.is_err());
+        server.verify().await;
+    }
+
+    #[tokio::test]
+    async fn test_coordinates_no_results() {
+        let server = MockServer::start().await;
+
+        let query = "NonExistentPlace";
+
+        let response_body = "[]";
+
+        Mock::given(method("GET"))
+            .and(path("/search.php"))
+            .and(query_param("format", "jsonv2"))
+            .and(query_param("q", query))
+            .respond_with(
+                ResponseTemplate::new(200).set_body_raw(response_body, "application/json"),
+            )
+            .mount(&server)
+            .await;
+
+        let result = coordinates(query).await;
+        assert!(result.is_err());
+        server.verify().await;
+    }
+}
